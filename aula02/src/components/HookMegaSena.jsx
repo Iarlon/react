379602a393b12482543356a5f27b33f9904babc8
iarlon <iarlonpvh@gmail.com>
2025-09-17 13:36:20 -0300

    adicionando atividade de hook02

--- aula02/src/components/HookMegaSena.jsx
+++ aula02/src/components/HookMegaSena.jsx
@@ -1,17 +1,31 @@
 import React, { useState } from 'react'
 
 const HookMegaSena = () => {
-    const [numeroSorteado, setNumeroSorteado] = useState()
-    const [numerosSorteados, setNumerosSorteados] = useState()
-    function sortearNumero() {
-        for (let i = 0; i < 6; i++){
-        Math.floor(Math.random() * 60) + 1
-        [...NumerosSorteados ,
-sorteado]
+  const [numeroSorteado, setNumeroSorteado] = useState()
+  const [numerosSorteados, setNumerosSorteados] = useState([])
+    
+  function sortearNumero() {
+    if(numerosSorteados.length < 6) {
+      const novoNumero = Math.floor(Math.random() * 60) + 1
+      setNumeroSorteado(novoNumero)
+      setNumerosSorteados([...numerosSorteados, novoNumero]);
     }
     }
   return (
-    <div>HookMegaSena</div>
+    <div>
+        <h1>sorteador de mega sena</h1>
+        <p>Clique para sortear</p>
+        <button onClick={() => sortearNumero()}>
+            clique
+        </button>
+        <h2>Numero atual {numeroSorteado}</h2>
+
+        <h2>Os numerosSorteados são: {numerosSorteados.join(' - ')}
+        </h2>
+
+        
+
+    </div>
   )
 }
 
