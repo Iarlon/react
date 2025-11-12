f04ed5be0024ad4c56f0c55d93cb9bb445fabeb8
Iarlon Soares da Silva Oliveira <Ialo@academicos.com>
2025-11-12 20:40:02 -0300

    Terminando aula06 e adicionando site-uniesp

--- aula06/src/pages/DpoLgpd.jsx
+++ aula06/src/pages/DpoLgpd.jsx
@@ -1,8 +1,29 @@
-import React from 'react'
+import {React, useEffect, useState} from 'react'
+import axios from 'axios'
 
 const DpoLgpd = () => {
+  const [mensagem, setMensagem] = useState([]);
+
+  useEffect(()=> {
+
+    axios.get('http://localhost:3000/dpo')
+    .then(r => setMensagem(r.data))
+    .catch(e => console.error("Não conseguiu carregara por algum motivo", e))
+
+  },[])
+
   return (
-    <div>Essa vai ser a página sobre o DPOLGPD</div>
+    <div>
+      <h1>DPO - LGPD</h1>
+        {
+          mensagem.map(i => (
+            <div key={i.id}>
+              <h2>{i.titulo}</h2><br></br>
+              <p>{i.mensagem} </p>
+            </div>  
+          ))
+        }
+    </div>
   )
 }
 
